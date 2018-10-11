f28b0e1fb213c637aaea9fafc216458a45f4cafe
NaunFlores <naunflores620@gmail.com>
2018-10-11 00:43:21 -0600

    creando modelo persona

--- app/Persona.php
+++ app/Persona.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Persona extends Model
 {
+    protected $table= 'persona';
     protected $fillable = ['nombre','apellidos', 'fechaDenacimiento', 'docuementoDeIdentidad', 'estadoCivil','genero'];
     
 }
