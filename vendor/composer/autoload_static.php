bab981427215a64374e0e852962da1294cea91a5
NaunFlores <naunflores620@gmail.com>
2018-10-11 02:25:02 -0600

    agregando modelo y migracion de HorarioDeClinica

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -364,6 +364,7 @@ class ComposerStaticInit8f8a07438a34d1bcee91781fb5874279
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
         'App\\Municipio' => __DIR__ . '/../..' . '/app/Municipio.php',
+        'App\\OfertaDeServicio' => __DIR__ . '/../..' . '/app/OfertaDeServicio.php',
         'App\\Paciente' => __DIR__ . '/../..' . '/app/Paciente.php',
         'App\\Persona' => __DIR__ . '/../..' . '/app/Persona.php',
         'App\\ProfesionalEnMedicina' => __DIR__ . '/../..' . '/app/ProfesionalEnMedicina.php',
