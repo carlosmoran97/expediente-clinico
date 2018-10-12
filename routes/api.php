fd65c23d76b0f98a0eab70b6978639720bf50250
Carlos Morán <carlosmoran.97cr@gmail.com>
2018-10-11 18:52:40 -0600

    agregar controlador de las especialiades

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/especialidad', 'EspecialidadController@index');
+Route::post('/especialidad', 'EspecialidadController@store');
+Route::put('/especialidad', 'EspecialidadController@update');
+Route::delete('/especialidad', 'EspecialidadController@destroy');
