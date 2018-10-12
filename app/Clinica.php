f9c483ee9d459efdbdaa35e50973be01b9252513
Carlos Morán <carlosmoran.97cr@gmail.com>
2018-10-11 21:01:17 -0600

    agregando la relacion con la direccion

--- app/Clinica.php
+++ app/Clinica.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Clinica extends Model
 {
-    protected $fillable = ['numero_registro', 'nombre', 'telefono', 'correo_electronico', 'persona_id'];
+    protected $fillable = ['numero_registro', 'nombre', 'telefono', 'correo_electronico', 'persona_id', 'direccion_id'];
     public function encargado(){
         return $this->belongsTo('App\Persona', 'persona_id');
     }
     public function personas(){
         return $this->hasMany('App\Persona');
+    }
+    public function direccion(){
+        return $this->belongsTo('App\Direccion', 'direccion_id');
     }
 }
--- app/Persona.php
+++ app/Persona.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Persona extends Model
 {
     protected $table= 'persona';
-    protected $fillable = ['nombre','apellidos', 'fechaDenacimiento', 'docuementoDeIdentidad', 'estadoCivil','genero'];
-    
+    protected $fillable = ['nombre','apellidos', 'fechaDenacimiento', 'docuementoDeIdentidad', 'estadoCivil','genero', 'direccion_id'];
+    public function direccion(){
+        return $this->belongsTo('App\Direccion', 'direccion_id');
+    }
 }
