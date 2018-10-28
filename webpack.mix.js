a6136ed1592bf98e6b93999831b4eee1e80cea0c
Eduardo Reye <reduardo.dev21@gmail.com>
2018-10-28 01:53:33 -0600

    Assets  configuracion

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,13 +11,21 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/App.js', 'public/js')
-    .react('resources/js/Login.js', 'public/js')
-   .sass('resources/sass/estilos-base.scss','public/css')
-   .sass('resources/sass/expediente-clinico.scss','public/css')
-   .sass('resources/sass/panel-admin.scss','public/css')
-   .sass('resources/sass/panel-registro-consulta.scss','public/css')
-   .sass('resources/sass/panel-superusuario.scss','public/css');
+mix.styles([
+    'resources/assets/plantilla/css/font-awesome.min.css',
+    'resources/assets/plantilla/css/simple-line-icons.min.css',
+    'resources/assets/plantilla/css/style.css',
+], 'public/css/plantilla.css')
+.scripts([
+    'resources/assets/plantilla/js/bootstrap.min.js',
+    'resources/assets/plantilla/js/Chart.min.js',
+    'resources/assets/plantilla/js/jquery.min.js',
+    'resources/assets/plantilla/js/pace.min.js',
+    'resources/assets/plantilla/js/popper.min.js',
+    'resources/assets/plantilla/js/template.js'
+
+], 'public/js/plantilla.js');
+
    
 
 
